974a2ba3dc487fc5ed3e5521d678ad1a028c7420
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-03-02 17:28:37 -0600

    Update script.js

--- Elaboración de Menú Hamburguesa (despliegue del lado derecho)/script.js
+++ Elaboración de Menú Hamburguesa (despliegue del lado derecho)/script.js
@@ -1,1 +1,10 @@
+// Obtiene el botón con el ID 'hamburger-btn' y agrega un evento de escucha al hacer clic
+document.getElementById('hamburger-btn').addEventListener('click', function() {
 
+    // Obtiene el elemento del menú lateral con el ID 'side-menu'
+    var sideMenu = document.getElementById('side-menu');
+
+    // Alterna la clase 'active' en el menú lateral
+    // Si la clase 'active' está presente, se elimina; si no está, se agrega
+    sideMenu.classList.toggle('active');
+});
