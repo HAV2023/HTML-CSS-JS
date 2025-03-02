3d2990b07373862bac4d382a0ba02bb9136f6b7f
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-03-02 17:34:06 -0600

    Update script.js

--- Elaboración de Menú Hamburguesa (despliegue partiendo del centro)/script.js
+++ Elaboración de Menú Hamburguesa (despliegue partiendo del centro)/script.js
@@ -1,1 +1,11 @@
+// Obtiene el botón con el ID 'hamburger-btn' y le agrega un evento de escucha para detectar clics
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+
+    // Obtiene el elemento del menú de pantalla completa con el ID 'full-screen-menu'
+    var fullScreenMenu = document.getElementById('full-screen-menu');
+
+    // Alterna la clase 'active' en el menú de pantalla completa
+    // Si la clase 'active' está presente, se elimina; si no está, se agrega
+    fullScreenMenu.classList.toggle('active');
+});
 
