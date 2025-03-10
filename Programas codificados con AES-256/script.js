479912ea31705921dd479544cb15811879d327c0
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-03-10 16:16:43 -0600

    Update script.js

--- Programas codificados con AES-256/script.js
+++ Programas codificados con AES-256/script.js
@@ -1,1 +1,6 @@
+const clave = "clave-super-secreta";
+const contenidoOriginal = "Este es el contenido secreto que nadie debe ver.";
+
+const contenidoCifrado = CryptoJS.AES.encrypt(contenidoOriginal, clave).toString();
+console.log("Contenido Cifrado:", contenidoCifrado);
 
