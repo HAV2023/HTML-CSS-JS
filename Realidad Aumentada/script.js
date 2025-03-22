c4a02c10cb7bd02fe69fca3e3f49d4ee1e1707f0
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-03-21 20:51:45 -0600

    Update script.js

--- Realidad Aumentada/script.js
+++ Realidad Aumentada/script.js
@@ -1,15 +1,24 @@
 // script.js
+
+// Se espera a que todos los recursos de la página se hayan cargado completamente
 window.addEventListener('load', function () {
+  
+  // Se selecciona el elemento <a-marker> de la escena, que representa el marcador AR
   var marker = document.querySelector('a-marker');
 
+  // Se añade un "listener" para el evento 'markerFound'
+  // Este evento se dispara cuando AR.js detecta el marcador en la cámara
   marker.addEventListener('markerFound', function() {
     console.log('¡Marker detectado!');
-    // Aquí puedes agregar lógica adicional al detectar el marcador.
+    // Aquí puedes agregar lógica adicional que se ejecute cuando se detecte el marcador,
+    // como mostrar información, activar animaciones, cargar contenido adicional, etc.
   });
 
+  // Se añade un "listener" para el evento 'markerLost'
+  // Este evento se dispara cuando AR.js deja de detectar el marcador en la cámara
   marker.addEventListener('markerLost', function() {
     console.log('Marker perdido');
-    // Aquí puedes agregar lógica adicional al perder la detección del marcador.
+    // Aquí puedes agregar lógica adicional que se ejecute cuando se pierda el marcador,
+    // como ocultar elementos, detener animaciones o reiniciar estados.
   });
 });
-
