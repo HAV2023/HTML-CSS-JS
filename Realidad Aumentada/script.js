33da3a271c460cfb36bfe39d77dc3b88a9b87092
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-03-21 20:30:40 -0600

    Update script.js

--- Realidad Aumentada/script.js
+++ Realidad Aumentada/script.js
@@ -1,1 +1,15 @@
+// script.js
+window.addEventListener('load', function () {
+  var marker = document.querySelector('a-marker');
+
+  marker.addEventListener('markerFound', function() {
+    console.log('¡Marker detectado!');
+    // Aquí puedes agregar lógica adicional al detectar el marcador.
+  });
+
+  marker.addEventListener('markerLost', function() {
+    console.log('Marker perdido');
+    // Aquí puedes agregar lógica adicional al perder la detección del marcador.
+  });
+});
 
