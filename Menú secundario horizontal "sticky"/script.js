2437985876ca74b170b4369a0c2490853c5429af
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-04-09 16:17:09 -0600

    Update script.js

--- Menú secundario horizontal "sticky"/script.js
+++ Menú secundario horizontal "sticky"/script.js
@@ -1,6 +1,13 @@
-// Este script se ejecuta cuando el DOM está completamente cargado
+// Se añade un listener al documento para el evento 'DOMContentLoaded'.
+// Este evento se activa cuando el contenido HTML ha sido completamente cargado y parseado,
+// sin necesidad de esperar a que se carguen recursos como imágenes o hojas de estilo.
 document.addEventListener('DOMContentLoaded', function() {
+  // Esta función se ejecuta una vez que el DOM está completamente listo.
+
+  // Se muestra un mensaje en la consola para indicar que el proceso de carga
+  // del contenido HTML ha finalizado correctamente.
   console.log("La página se ha cargado correctamente.");
   
-  // Puedes agregar funcionalidades adicionales aquí si es necesario.
+  // Aquí puedes agregar cualquier funcionalidad adicional que dependa de que el DOM
+  // ya esté totalmente cargado y listo para manipularse.
 });
