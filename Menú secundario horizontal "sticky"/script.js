d648485decf384ced8b48a0557678e29389a20ac
Hector Arciniega <125413020+HAV2023@users.noreply.github.com>
2025-04-09 16:00:36 -0600

    Update script.js

--- Menú secundario horizontal "sticky"/script.js
+++ Menú secundario horizontal "sticky"/script.js
@@ -1,1 +1,7 @@
+// Este script se ejecuta cuando el DOM está completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("La página se ha cargado correctamente.");
+  
+  // Aquí se pueden agregar funcionalidades adicionales si es necesario.
+});
 
